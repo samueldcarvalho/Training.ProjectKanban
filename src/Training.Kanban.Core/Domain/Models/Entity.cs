ea0c665014a305358ce05af2289a103763ab59fc
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-02-19 01:51:35 -0300

    Correção e ajustes na Classe Base de Entidades

---
+++ src/Training.Kanban.Core/Domain/Models/Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentValidation.Results;
+
+namespace Training.Kanban.Core.Models
+{
+    public abstract class Entity
+    {
+        public int Id { get; private set; }
+        public string Situacao { get; private set; }
+        public ValidationResult ValidationResult { get; protected set; }
+
+        protected Entity()
+        {
+            ValidationResult = new ValidationResult();
+            Situacao = "ATIVO";
+        }
+
+        public void AddValidationError(string error, string description)
+        {
+            ValidationResult.Errors.Add(new ValidationFailure(error, description));
+        }
+
+        public void Ativar() => Situacao = "ATIVO";
+        public void Inativar() => Situacao = "INATIVO";
+
+        public override bool Equals(object obj)
+        {
+            var comparteTo = obj as Entity;
+
+            if (ReferenceEquals(this, comparteTo)) return true;
+            if (ReferenceEquals(null, comparteTo)) return false;
+
+            return Id.Equals(comparteTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+    }
+}
