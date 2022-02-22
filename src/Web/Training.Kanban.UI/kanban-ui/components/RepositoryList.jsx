451fc6044ff22210ed1639b9d696cd6c76c7c06d
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-02-22 00:15:00 -0300

    Criação de novo projeto Next.Js para o front-end

---
+++ src/Web/Training.Kanban.UI/kanban-ui/components/RepositoryList.jsx
@@ -0,0 +1,13 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+
+        </section>
+    )
+}
