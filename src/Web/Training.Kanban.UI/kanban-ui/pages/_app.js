9c60c030a448159b2e2e27e3f539f9335243419e
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-02-24 23:46:06 -0300

    Criação do CSS da tela de Login

--- src/Web/Training.Kanban.UI/kanban-ui/pages/_app.js
+++ src/Web/Training.Kanban.UI/kanban-ui/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/bootstrap/bootstrap-grid.min.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/Web/Training.Kanban.UI/kanban-ui/pages/index.js
+++ src/Web/Training.Kanban.UI/kanban-ui/pages/index.js
@@ -1,13 +1,47 @@
-import Singin from "../components/singin/singin";
-
-
-
-export default function Entrar() {
+export default () => {
     return (
-        <div className="kb-sign">
-            <form className="kb-sign-form">
-                <Singin />
-            </form>
+        <div className="kb-login">
+            <header className="container kb-top-main-menu">
+                <div className="col-6 kb-top-main-menu-logo">
+                    Kanban
+                </div>
+            </header>
+            <div className="kb-sign-body">
+                <section className="kb-sign">
+                    <div className="kb-sign-change-form">
+                        <span>
+                            <p>You don't have <br /> an account?</p>
+                            <button className="kb-secundary-button">Sing-up</button>
+                        </span>
+                    </div>
+                    <form className="kb-sign-form">
+                        <div className="kb-sign-form-title">
+                            <p>Sign-in</p>
+                        </div>
+                        <div className="kb-sign-form-inputs">
+                            <label className="kb-sing-form-label" for="username">
+                                <p>Username</p>
+                                <input type="text" className="kb-sign-form-input-text" id="username"></input>
+                            </label>
+                            <label className="kb-sing-form-label" for="password">
+                                <p>Password</p>
+                                <input type="password" className="kb-sign-form-input-text" id="password"></input>
+                                <label className="kb-sing-form-label-remember-pass">
+                                    <p>Remember password?</p>
+                                    <input type="checkbox" ></input>
+                                </label>
+                            </label>
+                        </div>
+                        <div className="kb-sign-form-footer">
+                            <div>
+                                <p>Esqueceu a senha?&nbsp;</p>
+                                <a href="#">Resgatar</a>
+                            </div>
+                            <input className="kb-primary-button" type="submit" value="Login"></input>
+                        </div>
+                    </form>
+                </section>
+            </div>
         </div>
     );
 }
