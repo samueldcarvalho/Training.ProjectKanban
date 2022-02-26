3445b5e33edc699bd3399947a9035aee896816f2
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-02-26 19:33:09 -0300

    Alterações na tela home

--- src/Web/Training.Kanban.UI/kanban-ui/components/main/NavbarComponent.jsx
+++ src/Web/Training.Kanban.UI/kanban-ui/components/main/NavbarComponent.jsx
@@ -2,21 +2,21 @@ export default function NavbarComponent() {
     return (
         <>
             <header className="db-mainbar">
-                <div className="container">
-                    <div className="col">
+                <div className="container db-mainbar-container">
+                    <div className="col db-mainbar-logo-container">
                         <p>Kanban</p>
                     </div>
                 </div>
             </header>
             <header className="db-mainbar db-navbar">
                 <div className="container db-navbar-options">
-                    <div className="col">
+                    <div className="col-4 db-navbar-buttons">
                         <a className="db-navbar-button" href="">Home</a>
-                        <a className="db-navbar-button" href="">Boards</a>
                         <a className="db-navbar-button" href="">Teams</a>
+                        <a className="db-navbar-button" href="">Configuration</a>
                     </div>
 
-                    <div className="col db-navbar-end">
+                    <div className="col-2 db-navbar-end">
                         <a className="db-navbar-button-taskpoints" href="">Look for Taskpoints!</a>
                         <p>329</p>
                     </div>
