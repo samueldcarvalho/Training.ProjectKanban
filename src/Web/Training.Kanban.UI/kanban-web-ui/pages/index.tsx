9e98b17e5b3f6f519c753584ea906f688e7b155f
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-09 00:12:18 -0300

    Inclusão dos botões do submenu

--- src/Web/Training.Kanban.UI/kanban-web-ui/pages/index.tsx
+++ src/Web/Training.Kanban.UI/kanban-web-ui/pages/index.tsx
@@ -21,7 +21,11 @@ const Wrapper = (props: any) => {
                 <div className="container">
                     <div className="row">
                         <div className="col">
-
+                            <div className="kb-submenu-header-buttons">
+                                <label htmlFor="menu-button">
+                                    <a id="menu-button">Home</a>
+                                </label>
+                            </div>
                         </div>
                     </div>
                 </div>
