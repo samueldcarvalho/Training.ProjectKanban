324f99c85ad77a5eac74ca02bcf043863b4d7f17
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-10 19:00:17 -0300

    Remoção de pasta antiga do projeto UI

--- src/Web/Training.Kanban.UI/kanban-ui/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/styles.css';
-import '../styles/globals.css';
-import '../styles/bootstrap/bootstrap-grid.min.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
