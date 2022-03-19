d01c821337d37c3c3eb2808a4f5bb6d4a7ff406a
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-18 23:15:32 -0300

    Criação de UserContext, UserRepository e interfaces para inversão de Controle IoC

---
+++ src/Modules/Kanban/Training.Kanban.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Training.Kanban.Domain.Authentication.Models;
+
+namespace Training.Kanban.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Add(User entity);
+        void Update(User entity);
+        User GetById(int id);
+        void Dispose();
+    }
+}
--- src/Modules/Kanban/Training.Kanban.Infraestructure/DataContexts/Authentication/UserContext.cs
+++ src/Modules/Kanban/Training.Kanban.Infraestructure/DataContexts/Authentication/UserContext.cs
@@ -4,16 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Training.Core.Data.Repositories;
 using Training.Kanban.Domain.Authentication.Models;
 
 namespace Training.Kanban.Infraestructure.DataContexts.Authentication
 {
-    public class UserContext : DbContext
+    public class UserContext : DbContext, IUnitOfWork
     {
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
-
         }
+
         public DbSet<User> Users { get; set; }
+
+        public Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+                {
+                    entity.HasIndex(e => e.Email).IsUnique();
+                });
+        }
     }
 }
