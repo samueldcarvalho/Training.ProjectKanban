e7c049236a48c378c4c8fe8144b02aa72a9faf24
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-18 23:15:01 -0300

    Alteração na entidade User

--- src/Modules/Kanban/Training.Kanban.Domain/Authentication/Models/User.cs
+++ src/Modules/Kanban/Training.Kanban.Domain/Authentication/Models/User.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using Training.Core.Data.Repositories;
 using Training.Core.Domain.Models;
 
 namespace Training.Kanban.Domain.Authentication.Models
 {
-    public class User : Entity
+    public class User : Entity, IAggregateRoot
     {
-        public int Id { get; private set; }
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Password { get; private set; }
 
-        public User(int id, string name, string email, string password)
+        public User(string name, string email, string password)
         {
-            Id = id;
             Name = name;
             Email = email;
             Password = password;
