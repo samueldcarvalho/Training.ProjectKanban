999b98a2f4db478dde8a971c9c3d1c9c4fb0c330
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-18 23:13:41 -0300

    Alterações na Camada compartilhada (Core) para inclusão de IRepository, IUnitOfWork e IAggregateRoot, além da classe base de Entidade

---
+++ src/Core/Training.Core/Data/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training.Core.Data.Repositories
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+        void Add(T entity);
+        void Update(T entity);
+        T GetById(int id);
+    }
+}
--- src/Core/Training.Core/Domain/Models/Entity.cs
+++ src/Core/Training.Core/Domain/Models/Entity.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Training.Core.Domain.Models
 {
-    public class Entity
+    public abstract class Entity
     {
+        [Key]
+        public int Id { get; set; }
     }
 }
