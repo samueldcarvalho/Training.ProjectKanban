e510ffa26d6ab106b3106a94f607005679b33e5c
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-21 23:45:20 -0300

    Alteração no modalComponent

--- src/Web/Training.Kanban.UI/kanban-web-ui/src/components/main/Modal/modal-component.tsx
+++ src/Web/Training.Kanban.UI/kanban-web-ui/src/components/main/Modal/modal-component.tsx
@@ -1,10 +1,16 @@
-
+import CardTitleComponent from "../titlecard-component";
 
 const UIModal = (props: any) => {
     return (
         <div className="kb-modal-overlay">
-            <div className="kb-modal container">
-                {props.children}
+            <div className="kb-modal kb-card container">
+                <div className="kb-modal-header">
+                    <CardTitleComponent title={props.title} description={props.description} align="center" />
+                </div>
+
+                <div className="kb-modal-body">
+                    {props.children}
+                </div>
             </div>
         </div>
     );
--- src/Web/Training.Kanban.UI/kanban-web-ui/src/components/main/titlecard-component.tsx
+++ src/Web/Training.Kanban.UI/kanban-web-ui/src/components/main/titlecard-component.tsx
@@ -1,11 +1,12 @@
 type CardTitleProps = {
     title: string;
     description: string;
+    align: "flex-start" | "center" | "flex-end"
 }
 
 const CardTitleComponent = (props: CardTitleProps) => {
     return (
-        <div className="kb-card-title">
+        <div className="kb-card-title" style={{ alignItems: props.align.toString() }}>
             <p>{props.title}</p>
             <p>{props.description}</p>
         </div>
