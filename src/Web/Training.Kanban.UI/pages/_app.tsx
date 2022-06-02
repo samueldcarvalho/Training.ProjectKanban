cf8fc3a2e7a95a41e7bbeed8357321c1b636f2d4
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-06-01 23:12:22 -0300

    Configuração e implementação do Eslint Com Prettier

--- src/Web/Training.Kanban.UI/pages/_app.tsx
+++ src/Web/Training.Kanban.UI/pages/_app.tsx
@@ -1,15 +1,22 @@
-import '../src/styles/bootstrap/bootstrap-grid.min.css'
-import '../src/styles/globals.css'
-import '../src/styles/login-module.css'
-import '../src/styles/main-module.css'
-import '../src/styles/teams-module.css'
+import "../src/styles/bootstrap/bootstrap-grid.min.css";
+import "../src/styles/globals.css";
+import "../src/styles/login-module.css";
+import "../src/styles/main-module.css";
+import "../src/styles/teams-module.css";
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { TokenService } from "../src/services/Authentication/TokenService";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+export async function getServerSideProps(ctx: any) {
+  console.log(ctx);
+
+  return {
+    props: {},
+  };
+}
