77bb728292fffd549b54b1936f7b65dd300003aa
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-06-19 02:55:20 -0300

    Correção do ESLint

--- src/Web/Training.Kanban.UI/src/components/Shared/teamsgrid-component.tsx
+++ src/Web/Training.Kanban.UI/src/components/Shared/teamsgrid-component.tsx
@@ -1,32 +1,29 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
-import { Team } from "../../pages";
 
-const TeamGridComponent = (props: { teams: Team[] }) => {
-    return (
-        <>
-            <div>
-                <tr className="row kb-table-row kb-table-row-header" style={{ width: "100%" }}>
-                    <td className="col">Name</td>
-                    <td className="col">Task Points</td>
-                </tr>
-            </div>
-            {
-                props.teams.map(team => {
-                    return (
-                        <div>
-                            <tr className="row kb-table-row">
-                                <div >
-                                    <AiOutlineArrowRight />
-                                </div>
-                                <td className="col">{team.name}</td>
-                                <td className="col">{team.taskpoints}</td>
-                            </tr>
-                        </div>
-                    )
-                })
-            }
-        </>
-    );
-}
+const TeamGridComponent = (props: { teams: any }) => {
+  return (
+    <>
+      <div>
+        <tr className="row kb-table-row kb-table-row-header" style={{ width: "100%" }}>
+          <td className="col">Name</td>
+          <td className="col">Task Points</td>
+        </tr>
+      </div>
+      {props.teams.map((team: any) => {
+        return (
+          <div>
+            <tr className="row kb-table-row">
+              <div>
+                <AiOutlineArrowRight />
+              </div>
+              <td className="col">{team.name}</td>
+              <td className="col">{team.taskpoints}</td>
+            </tr>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
 export default TeamGridComponent;
