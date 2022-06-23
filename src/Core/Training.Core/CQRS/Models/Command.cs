2a11a3bd85baa40270c289b5bbb83d492f316867
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-06-23 13:53:43 -0300

    Criação de classes abstratas e interfaces para implementação da arquitetura CQRS

---
+++ src/Core/Training.Core/CQRS/Models/Command.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Training.Core.CQRS.Models
+{
+    public abstract class Command<T>
+    {
+        protected Task<bool> IsValid()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
